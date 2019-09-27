ae265a472002452515c500543e05ec6ce030c6e2
Hans Kesting <hans_kesting@epam.com>
2019-09-27 14:24:59 +0200

    trying parameterized test, but it fails

--- src/test/java/nl/hdkesting/javatwitter/accounts/EmailExistsTest.java
+++ src/test/java/nl/hdkesting/javatwitter/accounts/EmailExistsTest.java
@@ -5,16 +5,34 @@ import nl.hdkesting.javatwitter.accounts.services.AccountService;
 import nl.hdkesting.javatwitter.accounts.support.ConnStr;
 import nl.hdkesting.javatwitter.accounts.support.RequestBuilder;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.sql.SQLException;
 import java.util.*;
+import java.util.logging.Logger;
 
 import javax.management.InvalidApplicationException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(Parameterized.class)
 public class EmailExistsTest {
     private AccountService accountService;
+
+    @Parameterized.Parameters(name = "{index}: {0}->{1}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"unknown@invalid.com", HttpStatus.NOT_FOUND},
+                {"sample@example.com", HttpStatus.OK},
+                {"", HttpStatus.BAD_REQUEST}});
+    }
+
+    @Parameterized.Parameter(0)
+    public String emailAddressToTest;
+
+    @Parameterized.Parameter(1)
+    public HttpStatus expectedResponseStatus;
 
     // NB: do note that testing happens against an in-memory H2 database, while live runs against Azure SqlServer.
 
@@ -30,26 +48,14 @@ public class EmailExistsTest {
     }
 
     @Test
-    public void testEmailExists_unknownEmail() throws Exception {
-        assert performTest("unknown@invalid.com", HttpStatus.NOT_FOUND);
-    }
+    public void performTest() {
+        Logger.getGlobal().info("Testing address '" + this.emailAddressToTest + "' for status " + this.expectedResponseStatus);
 
-    @Test
-    public void testEmailExists_knownEmail() throws Exception {
-        assert performTest("sample@example.com", HttpStatus.OK);
-    }
-
-    @Test
-    public void testEmailExists_emptyEmail() throws Exception {
-        assert performTest("", HttpStatus.BAD_REQUEST);
-    }
-
-    private boolean performTest(String emailToTest, HttpStatus expectedResponse) {
         // ARRANGE
         initializeTest();
 
         final HttpRequestMessage<Optional<String>> req = new RequestBuilder()
-                .addQueryParameter("mail", emailToTest)
+                .addQueryParameter("mail", this.emailAddressToTest)
                 .build();
 
         // ACT
@@ -57,14 +63,11 @@ public class EmailExistsTest {
             final HttpResponseMessage ret = new EmailExists(this.accountService).run(req, RequestBuilder.getMockContext());
 
             // ASSERT
-            assertEquals(expectedResponse, ret.getStatus());
-            return true;
+            assertEquals(this.expectedResponseStatus, ret.getStatus());
         }
         catch (InvalidApplicationException ex) {
             ex.printStackTrace();
             fail();
         }
-
-        return false;
     }
 }
